91fd3dd35fe564df1b89de5c11a50b6f1066c3d2
gaidinX <kenet3@yahoo.com>
2014-02-08 00:23:07 +0800

    Add onclick handler.

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 simply.title("hey!");
+
+simply.on('singleClick', function(e) {
+  simply.subtitle('Beep ' + e.button + ' button!');
+});
+
+simply.on('longClick', function(e) {
+  simply.subtitle('Beeeeeeeeeeeeeeeeeeeep ' + e.button + ' button!');
+});
